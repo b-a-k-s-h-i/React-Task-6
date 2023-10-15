6196b748bd333aa7d65cd677f6e1777584f75bd4
Arnab Bakshi <94692230+b-a-k-s-h-i@users.noreply.github.com>
2023-10-15 22:04:14 +0530

    Add files via upload

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+
+
+
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <p>Email: arnabbakshi9@gmail.com</p>
+      <p>Phone: 94751184417</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/skills.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Skills() {
+  const skills = [
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'Express.js',
+    'MongoDB',
+    'SQL',
+    'PHP',
+    'C++',
+    'Python',
+    'BoothStrap'
+  ];
+
+  return (
+    <section className="skills">
+      <h2>Skills</h2>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
